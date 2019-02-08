237155973778f785a1df70ad40603f19036478ab
DESKTOP-FE0M562\yjs3694 <yjs3694@gmail.com>
2019-02-08 17:26:06 +0900

    ServerForm 클래스 멤버변수 선언 및 AppendText(string message) 메소드 작성

--- Server/Server/ServerForm.cs
+++ Server/Server/ServerForm.cs
@@ -6,19 +6,39 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Server
 {
     public partial class ServerForm : Form
     {
+        delegate void AppendTextDelegate(string s);
+        AppendTextDelegate textAppender;
+        Socket serverSocket;
+        IPAddress thisAddress;
+        List<Socket> connectClientList;
+
         public ServerForm()
         {
             InitializeComponent();
         }
 
+        // Form Load 시 초기화
         private void ServerForm_Load(object sender, EventArgs e)
         {
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            textAppender = new AppendTextDelegate(AppendText);
+
 
         }
+
+        // 메시지, 상태 등의 내역 쓰기
+        private void AppendText(string message)
+        {
+            if (textStatus.InvokeRequired) textStatus.Invoke(textAppender, message);
+            else textStatus.Text += "\r\n" + message;
+        }
     }
+
 }
