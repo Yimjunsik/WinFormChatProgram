e0858ac1c8de74e0b81a32927186ef84ea4da361
DESKTOP-FE0M562\yjs3694 <yjs3694@gmail.com>
2019-02-08 15:17:28 +0900

    디자이너를 통한 Label 생성

--- Server/Server/ServerForm.Designer.cs
+++ Server/Server/ServerForm.Designer.cs
@@ -28,13 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelAddress = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.labelAddress.Location = new System.Drawing.Point(3, 0);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(94, 33);
+            this.labelAddress.TabIndex = 1;
+            this.labelAddress.Text = "서버 주소";
+            this.labelAddress.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // ServerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelAddress);
             this.Name = "ServerForm";
             this.Text = "Chatting by junsik - Server";
             this.Load += new System.EventHandler(this.ServerForm_Load);
@@ -43,6 +55,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelAddress;
     }
 }
 
