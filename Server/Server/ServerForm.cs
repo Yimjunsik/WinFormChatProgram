dc646a9d4d02250c3ebfa387b40b51f7fe593777
DESKTOP-FE0M562\yjs3694 <yjs3694@gmail.com>
2019-02-09 19:49:11 +0900

    1. Callback에 대한 내용 저장을 위한 Class인 AsyncObject 작성 완료. 2. AcceptCallback() 및 buttonConnect_Click() 메소드 작성 중

--- Server/Server/ServerForm.cs
+++ Server/Server/ServerForm.cs
@@ -47,6 +47,59 @@ namespace Server
             dataGridView.ReadOnly = true;
         }
 
+        // 시작 버튼 클릭 시 연결 시작
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            int port;
+            if (serverSocket == null)
+            {
+                serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                connectClientList = new List<Socket>();
+            }
+            try { port = Int32.Parse(textPort.Text); }
+            catch
+            {
+                MessageBox.Show("포트 번호가 잘못 입력되었습니다.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textPort.Focus();
+                textPort.SelectAll();
+                return;
+            }
+
+            if (serverSocket.IsBound)
+                MessageBox.Show("서버가 실행 중입니다.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else if (port < 0 || port > 65535)
+            {
+                MessageBox.Show("포트 번호가 잘못 입력되었습니다.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textPort.Focus();
+                textPort.SelectAll();
+            }
+            else
+            {
+                IPEndPoint endPoint = new IPEndPoint(thisAddress, port);
+                serverSocket.Bind(endPoint);
+                serverSocket.Listen(20);
+
+                serverSocket.BeginAccept(acceptcallback)
+            }
+        }
+
+        // Client에서 연결 신호가 들어오면 시작되는 Callback
+        private void AcceptCallback(IAsyncResult asyncResult)
+        {
+            try
+            {
+                Socket client = serverSocket.EndAccept(asyncResult);
+
+                serverSocket.BeginAccept(AcceptCallback, null);
+
+                Ayncobje
+
+                
+
+            }
+            catch { }
+        }
+
         // 메시지, 상태 등의 내역 쓰기
         private void AppendText(string message)
         {
@@ -55,4 +108,23 @@ namespace Server
         }
     }
 
+    // Callback에 대한 내용 저장을 위한 Class
+    public class AsyncObject
+    {
+        public byte[] Buffer;
+        public Socket WorkingSocket;
+        public readonly int BufferSize;
+
+        public AsyncObject(int bufferSize)
+        {
+            BufferSize = bufferSize;
+            Buffer = new byte[BufferSize];
+        }
+
+        public AsyncObject(int buffersize, Socket tempSocket)
+            : this(buffersize) { WorkingSocket = tempSocket; }
+
+        public void ClearBuffer() { Array.Clear(Buffer, 0, BufferSize); }
+
+    }
 }
