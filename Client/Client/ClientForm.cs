3951ebc79ed74c5db674647fb68e83288e2a1e60
DESKTOP-FE0M562\yjs3694 <yjs3694@gmail.com>
2019-02-10 21:59:44 +0900

    Form Load 시 초기화를 위한 생성자 ClientForm(object sender, EventArgs e) 추가.

--- Client/Client/ClientForm.cs
+++ Client/Client/ClientForm.cs
@@ -15,11 +15,31 @@ namespace Client
     {
         delegate void AppendTextDelegate(string s);
         AppendTextDelegate textAppender;
+        Socket serverSocket;
 
-        public ClientForm()
+        public ClientForm() { InitializeComponent(); }
+        
+        // Form Load 시 초기화
+        public ClientForm(object sender, EventArgs e)
         {
-            InitializeComponent();
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            textAppender = new AppendTextDelegate(AppendText);
+
+            IPHostEntry hostEntry = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress defaultAddress = null;
+            foreach (IPAddress addr in hostEntry.AddressList)
+            {
+                if (addr.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    defaultAddress = addr;
+                    break;
+                }
+            }
+
+            if (defaultAddress == null) defaultAddress = IPAddress.Loopback;
+            textAddress.Text = defaultAddress.ToString();
         }
+
 
         // 메세지, 상태 등의 내역 쓰기
         private void AppendText(string message)
