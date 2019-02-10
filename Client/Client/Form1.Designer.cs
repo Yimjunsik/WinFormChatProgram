40b2e4e58063602a3c229c16f0968386f511aa88
DESKTOP-FE0M562\yjs3694 <yjs3694@gmail.com>
2019-02-10 15:54:10 +0900

    ClientForm 사이즈 속성 수정

--- Client/Client/Form1.Designer.cs
+++ Client/Client/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(584, 441);
             this.Name = "ClientForm";
             this.Text = "Chatting by junsik - Client";
             this.ResumeLayout(false);
